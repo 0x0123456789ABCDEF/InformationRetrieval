436fcfe6da497872c6b482804145de8e0e3d67a1
Alexey Savenkov <alexey199753@gmail.com>
2017-01-21 21:45:47 +0200

    Practice1 now prints total number of characters in all files

--- src/main/scala-2.12/practice1/Practice1.scala
+++ src/main/scala-2.12/practice1/Practice1.scala
@@ -11,33 +11,42 @@ import scala.collection.immutable.SortedMap
   * @author alosha
   */
 object Practice1 extends App {
+
+      val bookNames = (1 to 10).map("practice1/book" + _ + ".txt")
+
       val reverseIndex = SortedMap(
-        (1 to 10).map("practice1/book" + _ + ".txt").par.map(documentName => {
-          Source.fromResource(documentName).getLines().zipWithIndex.flatMap({
-            case (line, lineIndex) => line.split("[^\\w]+").map(_.toLowerCase -> lineIndex)
-          }).toStream.groupBy(_._1).mapValues(_.map(documentName -> _._2))
+        bookNames.par.map(bookName => {
+          Source.fromResource(bookName).getLines().zipWithIndex.flatMap({
+            case (line, lineIndex) => line.split("[^\\w]+").filter(!_.isEmpty).map(_.toLowerCase -> lineIndex)
+          }).toStream.groupBy(_._1).mapValues(_.map(bookName -> _._2))
         }).reduce(_ |+| _).toSeq : _*
       )
 
-      val reverseIndexPrinted =
+      val formatInt = java.text.NumberFormat.getIntegerInstance.format(_ : Long)
+
+      val reverseIndexPrettyPrint =
         reverseIndex.mapValues(_.sorted.map({ case (doc, line) => s"Document $doc; line $line\n" }).mkString).map({
           case (word, result) =>
             s"""----- WORD '$word' -----
-                | Occurances: ${reverseIndex(word).size}
+                | Occurances: ${formatInt(reverseIndex(word).size)}
                 |
                 |$result
                 |
                 |""". stripMargin
         }).mkString
 
+
       val result =
         s"""
-           | Unique words count: ${reverseIndex.size}
-           | Total words count: ${reverseIndex.map(_._2.size).sum}
+           | Total character count: ${formatInt(bookNames.par.map(Source.fromResource(_)).map(_.iter.size.toLong).sum)}
+           | Total words count: ${formatInt(reverseIndex.map(_._2.size).sum)}
+           | Unique words count: ${formatInt(reverseIndex.size)}
            |
-           | $reverseIndexPrinted
+           | Reverse index:
+           |
+           | $reverseIndexPrettyPrint
            |
          """.stripMargin
 
-      new PrintWriter("output_1.txt") { write(result); close }
+      new PrintWriter("output_1.txt") { write(result); close() }
 }
